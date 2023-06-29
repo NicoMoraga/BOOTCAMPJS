41ad49128fef92d005a812a6b6efe93914117642
NicoMoraga~ <nicomora1998@gmail.com>
2023-06-29 11:49:00 -0400

    tarea de forms

---
+++ formulario/js.js
@@ -0,0 +1,8 @@
+const formularioDom = document.querySelector("#formm");
+
+formularioDom.addEventListener("submit", function(e){
+    e.preventDefault();
+console.log("evento ok");
+alert("se envia tu formulario");
+form.reset
+})
